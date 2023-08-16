0f019756ad12c88aa049d09e1e5858fe8eb1fca0
Cecil Andibanbang <andibanbangcecil@gmail.com>
2023-08-16 11:26:07 +0100

    Rail Tasks Manger

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,15 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get '/tasks', to: 'tasks#index'
+
+  get 'tasks/new', to: 'tasks#new'
+  post 'tasks', to: 'tasks#create'
+
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :task_edit
+  post 'tasks/:id', to: 'task#update'
+
+  get 'tasks/:id', to: 'tasks#show', as: :task
+
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
